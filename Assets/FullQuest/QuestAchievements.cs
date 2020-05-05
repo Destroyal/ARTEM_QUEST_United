660773efffef8cb0f88f7e0187dabc8ec3d8bb20
Destroyal <destroyaltv@gmail.com>
2020-05-05 16:20:23 +0200

    Travail0505

---
+++ Assets/FullQuest/QuestAchievements.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QuestAchievements : MonoBehaviour
+{
+    //Hints -- Default Value = false
+    
+    //Prévoir un délai de fermeture
+    public bool must_inventory; //Le joueur se voit proposer d'ouvrir son inventaire
+    public bool has_inventory; //Le joueur a ouvert son inventaire
+
+    public bool must_select; //Le joueur se voit proposer de sélectionner un item dans son inventaire
+    public bool has_selected; //Le joueur a sélectionné l'objet
+
+    public bool RunBoyRun; //L'intitulé de la quête apparaît : Trouver le trésor d'ARTEM avant la prochaine lune
+
+    public bool must_equip; // Le joueur se voit proposer d'équipper un certain équipement
+    public bool has_equip; // 
+
+    //Achievement
+    public 
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
